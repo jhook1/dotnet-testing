dd92bbb504b648c96a54f6dba06335ffd074d354
John Hook <jhook1@ufl.edu>
2022-09-25 08:46:20 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.MapGet("/", () => "Hello World!");
+app.MapFallbackToFile("index.html");
+
+app.Run();
