9c5bd03784b0e4af9b236c58a52bb9f67388a8f3
John Hook <jhook1@ufl.edu>
2022-09-25 12:09:04 -0700

    Some simple test routes/handlers

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using dotnet_testing;
 
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
 app.UseStaticFiles();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/stuff", () => { Console.WriteLine("Get!"); return "Hello World!"; });
+app.MapGet("/stuff/{id}", (int id) => $"Stuff #: {id}");
+app.MapPost("/stuff", (SimpleModel obj) => { 
+    Console.WriteLine("Post!");
+    return $"Obj ({obj.Id}) Name: {obj.Name} Description: {obj.Description}"; 
+});
+
 app.MapFallbackToFile("index.html");
 
 app.Run();
---
+++ SimpleModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_testing
+{
+    public class SimpleModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
