de53d6d7eca633b16dd80fd1b0658445e1e52e4a
John Hook <jhook1@ufl.edu>
2022-09-25 15:40:48 -0700

    Created basic html form to submit post

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using dotnet_testing;
 
 var builder = WebApplication.CreateBuilder();
@@ -12,6 +16,13 @@ app.MapGet("/stuff/{id}", (int id) => $"Stuff #: {id}");
 app.MapPost("/stuff", (SimpleModel obj) => { 
     Console.WriteLine("Post!");
     return $"Obj ({obj.Id}) Name: {obj.Name} Description: {obj.Description}"; 
+});
+app.MapGet("/stuff/form", (HttpContext context) =>
+{
+    string htmlText = File.ReadAllText("wwwroot/post.html");
+    context.Response.ContentType = MediaTypeNames.Text.Html;
+    context.Response.ContentLength = Encoding.UTF8.GetByteCount(htmlText);
+    context.Response.WriteAsync(htmlText);
 });
 
 app.MapFallbackToFile("index.html");
